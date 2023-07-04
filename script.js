4fbbc75ba7fe8863ba9714c20b132bd3fc1ceba2
MartinCL1 <116780330+MartinCL1@users.noreply.github.com>
2023-07-04 17:04:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,30 @@ function centrarse(elemento) {
 	elemento.scrollIntoView({ behavior: 'smooth', block: 'center' });
 }
 
-function operar(contenedor){   // Se activara siempre y cuando presionemos el boton para agregar un elemento al carrito
-	// let nombre=  contenedor.querySelector('h2').innerHTML;
+let bandera = false
+
+function operar(contenedor) {   // Se activara siempre y cuando presionemos el boton para agregar un elemento al carrito
+	// let nombre=  contenedor.querySelector('h2').innerHTML
+	comprar()
 	let descripcion = contenedor.querySelector('h3').innerHTML;
 	let talla = contenedor.querySelector('select');
 	let tallas = talla.options
-	console.log(`Datos: \n\n${descripcion}, ${tallas[talla.selectedIndex].innerHTML}`)
+
+	let titulo = document.getElementById('titulo-carrito')
+	titulo.innerHTML = `Se agrego correctamente<br> ${descripcion}`
+	let info = document.getElementById('info')
+	info.innerHTML = `Talla: ${tallas[talla.selectedIndex].innerHTML}
+	precio: 350.20`
+}
+
+let comprar = function () {
+	let modal = document.getElementById('modal')
+	modal.classList.add('modal-tienda')
+	modal.classList.remove('modal-tienda-hidden')
+	bandera = true
+}
+
+let cerrar = function () {
+		let modal = document.getElementById('modal')
+		modal.classList.add('modal-tienda-hidden')
 }
